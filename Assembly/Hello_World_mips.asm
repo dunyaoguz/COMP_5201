03fd7d02baeb4df20a4b986a225b429e0081d613
SanteauX <hugo.joncour.k2o@gmail.com>
2020-12-14 19:09:43 +0100

    enlève assembleur

--- Assembly/Hello_World_mips.asm
+++
@@ -1,9 +0,0 @@
-.data		#The information is stored in Random Access Memory
-
-		myMessage: .asciiz "Hello World !\n"
-
-.text		#Contains the instructions
-		
-	li $v0, 4		#li: Load Immediate
-	la $a0, myMessage	#la: Load Address
-	syscall
